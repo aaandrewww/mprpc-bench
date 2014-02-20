00848efb67052cc92cc977b30dda747c26a11536
Daniel King <daniel.zidan.king@gmail.com>
2014-02-19 21:28:19 -0500

    More useful ranges

--- runWindowedMessageSizeTest.sh
+++ runWindowedMessageSizeTest.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 for run in `seq 0 10`; do
-    for count in 1000000 10000000 50000000 100000000; do
-        for size in 0 10 100 1000; do
-            for window in 10 100 1000 10000 100000 1000000; do
-                if [[ "$window" -le "$size" ]]; then
+    for count in 1000000 10000000 100000000 1000000000; do
+        # for size in 0; do
+        size=0
+            for window in 10000 50000 100000 500000 1000000 10000000 100000000; do
+                if [[ "$window" -le "$count" ]]; then
                     echo -e -n "$count\t$size\t$window\t"
                     ./windowed-message-size -m $size -n $count -w $window -q
                 fi
             done
-        done
+        # done
     done
 done
